92185d252342ee88e8f5df7f27a56401e7530d36
Prashant Thevar <prashant.thevar@gmail.com>
2024-12-03 23:02:14 +0530

    update port

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello, Railway!");
-app.MapGet("/user", () => new { Id = 1, Name = "Jane Doe" });
+// Use the environment variable PORT if available, otherwise fallback to 5000
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+app.MapGet("/", () => "Hello, World!");
 
-app.Run();
+app.Run($"http://0.0.0.0:{port}");
